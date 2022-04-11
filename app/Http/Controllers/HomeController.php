e214d98fc0ee2b262f585850a0306a01b1a1910b
Medkaffouh <kaffouhmohamed@gmail.com>
2022-04-11 22:05:08 +0000

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Region;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $regions = Region::all();
+        return view('welcome', compact('regions'));
+
+    }
+
+    public function getNational(Request $request){
+        $region_id = $request->region_id;
+
+        return $data = DB::table('salles')
+        ->join('users','salles.user_id','users.id')
+        ->where('users.region_id',$region_id)
+        ->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/ourJs.blade.php
@@ -0,0 +1,21 @@
+<script>
+$(document).ready(function(){
+    //get region id
+    $("#regionID").click(function(){
+
+        var region = $("#regionID").val();
+        //console.log(cat);
+    $.ajax({
+      type: 'get',
+      dataType: 'html',
+      url: '{{url('/getUniversiteNational')}}',
+      data: 'region_id=' + region,
+      success:function(response){
+        console.log(response);
+        $("#productData").html(response);
+      }
+    });
+
+    });
+});
+</script>
